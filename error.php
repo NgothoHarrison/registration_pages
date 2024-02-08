0671d6cc56a50c30e98c4f73c3b990ad9d8556b2
Hank <113780705+NgothoHarrison@users.noreply.github.com>
2024-02-08 23:54:49 +0300

    Update error.php

--- error.php
+++ error.php
@@ -2,7 +2,7 @@
     <body>
         <div class="error">
             <h1 style="text-align: center; color:red">
-                ERROR 404
+                ERROR 404 
             </h1>
         </div>
     </body>
